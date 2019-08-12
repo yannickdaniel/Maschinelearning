9c9664007424f4863d4618bd971ba78e594d0257
Yannick Daniel <yannick-daniel@outlook.de>
2019-08-12 12:54:48 +0200

    Evaluation Model auswählen

--- test.py
+++ test.py
@@ -36,3 +36,54 @@ print(describe)
 groups = dataset.groupby('class').size()
 print(groups)
 
+#Plots
+dataset.plot(kind= 'box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+#Histogram
+dataset.hist()
+plt.show()
+
+#Scatterplots
+scatter_matrix(datset)
+plt.show()
+
+#Evaluate some Algorithms
+#Aufteilung in Trainings- und Testdaten
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,Y, test_size = validation_size, random_state = seed)
+
+
+#Build Models:
+#Welches Model ist am besten geeignet?
+#Logistic Regression (LR)
+#Linear Discriminant Analysis (LDA)
+#K-Nearest Neighbors (KNN).
+#Classification and Regression Trees (CART).
+#Gaussian Naive Bayes (NB).
+#Support Vector Machines (SVM).
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class= 'ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+results = []
+names = []
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits = 10, random_state = seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
